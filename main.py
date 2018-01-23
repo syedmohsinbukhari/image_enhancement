5f81ab8ebe47e5fcf5ba8b930656763863330d1b
Syed Mohsin Bukhari <syedmohsinbukhari@googlemail.com>
2018-01-23 16:15:13 +0500

    Fixing GAN

--- main.py
+++ main.py
@@ -36,10 +36,10 @@ class GAN:
 
         #Get Generator and Discriminator Outputs
         self.G_z, self.G_z_feats = self.generator(self.Z)
-        self.D_x, self.D_x_logit = self.discriminator(self.X, 'real')
-        self.D_z, self.D_z_logit = self.discriminator(self.G_z, 'fake')
+        self.D_x, self.D_x_logit = self.discriminator(self.X)
+        self.D_z, self.D_z_logit = self.discriminator(self.G_z, reuse=True)
 
-        #Defining Loss Functions
+        #Defining Loss Functions (Alternative Loss Functions just before EOF)
         generated_flat = tf.reshape(self.G_z, [self.batch_size, -1])
         original_flat = tf.reshape(self.X, [self.batch_size, -1])
         self.generation_loss = -tf.reduce_sum(original_flat * tf.log(1e-8 + \
@@ -47,18 +47,8 @@ class GAN:
             generated_flat), 1)
         self.cost = tf.reduce_mean(self.generation_loss)
 
-#        self.D_loss=-tf.reduce_mean(tf.log(self.D_x)+tf.log(1.0 - self.D_z))
-#        self.G_loss=-tf.reduce_mean(tf.log(self.D_z))+self.latent_loss
-
-        # Alternative losses:
-        # -------------------
-        D_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(\
-                logits=self.D_x_logit, labels=tf.ones_like(self.D_x_logit)))
-        D_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(\
-                logits=self.D_z_logit, labels=tf.zeros_like(self.D_z_logit)))
-        self.D_loss = D_loss_real + D_loss_fake
-        self.G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(\
-                logits=self.D_z_logit, labels=tf.ones_like(self.D_z_logit)))
+        self.D_loss=-tf.reduce_mean(tf.log(self.D_x)+tf.log(1.0 - self.D_z))
+        self.G_loss=-tf.reduce_mean(tf.log(self.D_z))
 
         #Prepare variable lists
         t_vars = tf.trainable_variables()
@@ -76,11 +66,11 @@ class GAN:
                                 minimize(self.cost, var_list = self.G_vars)
 
 
-    def discriminator(self, x, scope, reuse=False):
-        if reuse:
-            tf.get_variable_scope().reuse_variables()
+    def discriminator(self, x, reuse=False):
+        with tf.variable_scope("discriminator") as scope:
+            if reuse:
+                scope.reuse_variables()
 
-        with tf.variable_scope(scope):
             #Convolutional Layers
             D_conv1 = conv2d(x, 16, 3, 'D_conv1', strides=2)
             D_conv2 = conv2d(D_conv1, 32, 3, 'D_conv2', strides=2)
@@ -92,48 +82,48 @@ class GAN:
             D_pool5_flat = tf.layers.flatten(D_conv5)
 
             #Output
-            D_dense = dense(inputs=D_pool5_flat, units=2048, name='D_dense')
-            D_logits = dense(inputs=D_dense, units=1, name='D_logits',
-                             activation=None)
+            D_dense = dense(D_pool5_flat, 2048, 'D_dense')
+            D_logits = dense(D_dense, 1, 'D_logits', activation=None)
             D_probs = tf.nn.softmax(D_logits, name="D_probs")
 
             return D_probs, D_logits
 
 
     def generator(self, z):
-        ## Encoder
-        #Convolution Layers
-        G_conv1 = conv2d(z, 16, 3, 'G_conv1', strides=2)
-        G_conv2 = conv2d(G_conv1, 32, 3, 'G_conv2', strides=2)
-        G_conv3 = conv2d(G_conv2, 64, 3, 'G_conv3', strides=2)
-        G_conv4 = conv2d(G_conv3, 128, 3, 'G_conv4', strides=2)
-        G_conv5 = conv2d(G_conv4, 256, 3, 'G_conv5')
+        with tf.variable_scope("generator"):
+            ## Encoder
+            #Convolution Layers
+            G_conv1 = conv2d(z, 16, 3, 'G_conv1', strides=2)
+            G_conv2 = conv2d(G_conv1, 32, 3, 'G_conv2', strides=2)
+            G_conv3 = conv2d(G_conv2, 64, 3, 'G_conv3', strides=2)
+            G_conv4 = conv2d(G_conv3, 128, 3, 'G_conv4', strides=2)
+            G_conv5 = conv2d(G_conv4, 256, 3, 'G_conv5')
 
-        #Flattening
-        G_pool5_flat = tf.layers.flatten(G_conv5)
+            #Flattening
+            G_pool5_flat = tf.layers.flatten(G_conv5)
 
-        #Output
-        G_features = dense(inputs=G_pool5_flat, units=512, name='G_features')
+            #Output
+            G_features = dense(G_pool5_flat, 512, 'G_features')
 
-        ## Decoder
-        #Reshapping
-        flat_sz = int(IMG_DIM[0]*IMG_DIM[1]*256*(2**-8))
-        G_z_dev = dense(inputs=G_features, units=flat_sz, name='G_z_dev')
+            ## Decoder
+            #Reshapping
+            flat_sz = int(IMG_DIM[0]*IMG_DIM[1]*256*(2**-8))
+            G_z_dev = dense(G_features, flat_sz, 'G_z_dev')
 
-        G_z_mat = tf.reshape(G_z_dev, tf.shape(G_conv5))
-        G_z_matrix = tf.nn.relu(G_z_mat, name='G_z_matrix')
+            G_z_mat = tf.reshape(G_z_dev, tf.shape(G_conv5))
+            G_z_matrix = tf.nn.relu(G_z_mat, name='G_z_matrix')
 
-        #Transpose Convolutional Layers
-        G_conv_t_1 = conv2d_t(G_z_matrix, 256, 3, 'G_conv_t_1')
-        G_conv_t_2 = conv2d_t(G_conv_t_1, 128, 3, 'G_conv_t_2')
-        G_conv_t_3 = conv2d_t(G_conv_t_2, 64, 3, 'G_conv_t_3')
-        G_conv_t_4 = conv2d_t(G_conv_t_3, 32, 3, 'G_conv_t_4')
-        G_conv_t_5 = conv2d_t(G_conv_t_4, IMG_CHN, 3, 'G_conv_t_5', strides=1,
-                              activation=tf.nn.tanh)
+            #Transpose Convolutional Layers
+            G_conv_t_1 = conv2d_t(G_z_matrix, 256, 3, 'G_conv_t_1')
+            G_conv_t_2 = conv2d_t(G_conv_t_1, 128, 3, 'G_conv_t_2')
+            G_conv_t_3 = conv2d_t(G_conv_t_2, 64, 3, 'G_conv_t_3')
+            G_conv_t_4 = conv2d_t(G_conv_t_3, 32, 3, 'G_conv_t_4')
+            G_conv_t_5 = conv2d_t(G_conv_t_4, IMG_CHN, 3, 'G_conv_t_5',
+                                  strides=1, activation=tf.nn.tanh)
 
-        G_pic = tf.divide(tf.add(G_conv_t_5, 1.0), 2.0)
+            G_pic = tf.divide(tf.add(G_conv_t_5, 1.0), 2.0)
 
-        return G_pic, G_features
+            return G_pic, G_features
 
 
     def train(self, epochs, tf_session):
@@ -150,27 +140,29 @@ class GAN:
                                     CV_IMG_TYPE, IMG_CHN, IMG_DIM)
 
             for d in data_cur:
-#                for k in range(2):
-#                    _, D_loss_cur = sess.run([self.D_optimizer,
-#                                              self.D_loss], \
-#                                              feed_dict={self.X: d[1], \
-#                                                         self.Z: d[0]})
-#                for k in range(1):
-#                    _, G_loss_cur = sess.run([self.G_optimizer, \
-#                                              self.G_loss], \
-#                                              feed_dict={self.Z: d[0]})
+                for k in range(2):
+                    _, D_loss_cur, D_x = sess.run([self.D_optimizer,
+                                                   self.D_loss,
+                                                   self.D_x],
+                                                   feed_dict={self.X: d[1],
+                                                              self.Z: d[0]})
                 for k in range(1):
-                    _, generation_loss =  sess.run([self.AE_optimizer, \
-                                          self.cost], \
-                                          feed_dict={self.X: d[1], \
-                                                     self.Z: d[0]})
+                    _, G_loss_cur, D_z = sess.run([self.G_optimizer,
+                                                   self.G_loss,
+                                                   self.D_z],
+                                                   feed_dict={self.Z: d[0]})
+#                for k in range(1):
+#                    _, generation_loss =  sess.run([self.AE_optimizer, \
+#                                          self.cost], \
+#                                          feed_dict={self.X: d[1], \
+#                                                     self.Z: d[0]})
 
             if i%1==0:
                 print('Epoch: {}'.format(i))
-#                print('D loss: {0}'.format(D_loss_cur))
-#                print('G_loss: {0}'.format(G_loss_cur))
-                print('mean_generation_loss: {0}'.format(np.mean(
-                        generation_loss)))
+                print('D loss: {0}\nD_x: {1}'.format(D_loss_cur, D_x))
+                print('G_loss: {0}\nD_z: {1}'.format(G_loss_cur, D_z))
+#                print('mean_generation_loss: {0}'.format(np.mean(
+#                        generation_loss)))
                 print()
 
             self.test(sess, epoch='epoch_'+str(i))
@@ -247,3 +239,13 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+#        # Alternative losses:
+#        # -------------------
+#        D_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(\
+#                logits=self.D_x_logit, labels=tf.ones_like(self.D_x_logit)))
+#        D_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(\
+#                logits=self.D_z_logit, labels=tf.zeros_like(self.D_z_logit)))
+#        self.D_loss = D_loss_real + D_loss_fake
+#        self.G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(\
+#                logits=self.D_z_logit, labels=tf.ones_like(self.D_z_logit)))
